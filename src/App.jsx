3bc6dc7f22abe14cfbfa07e533d7d387c18c1eb0
acharyanisha1234 <acharyanisha2063gmail.com>
2025-07-14 09:53:37 +0545

    prot Route Completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,21 @@ import Loginform from "./pages/Login.jsx";
 import Home from "./pages/home.jsx";
 import Employeeform from "./pages/Employee.jsx";
 import Dashboardform from "./pages/Dashboard.jsx"
-
+import ProtectedRoute from "./components/home/Utils/protectedRoute.jsx";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Loginform />} />
-        <Route path="home" element={<Home />} >
+        <Route path="home" 
+        element={
+        <ProtectedRoute>
+          <Home />
+        </ProtectedRoute>
+     }
+     >
+    
           <Route path="employee" element={<Employeeform />} />
           <Route path="Dashboard" element={<Dashboardform />} />
         </Route>
